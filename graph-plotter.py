5096148a4a39bf96b7b589ce3cf4ce58d59a1e42
Amirkhon <71869548+AAgzamov@users.noreply.github.com>
2021-07-27 15:18:30 +0500

    Create graph-plotter.py

---
+++ graph-plotter.py
@@ -0,0 +1,54 @@
+print('\n\t\tLoading...\n')
+
+# IMPORTING FUNCTIONS FFORM "functions.py" FILE.
+import functions as f
+
+# MAIN PROGRAM LOOP.
+while 1:
+    f.clear()
+    f.menu()
+    user = input()
+    if user == 'exit()':
+        f.exit()
+    elif user == '1':
+        f.clear()
+        f.menu_1()
+        user = input()
+        if user == 'exit()':
+            f.exit()
+        elif user == '1':
+            f.clear()
+            f.types_of_graphs()
+            user = input()
+            if user == 'exit()':
+                f.exit()
+            elif user == '1':
+                f.clear()
+                f.line_graph()
+                f.clear()
+            elif user == '2':
+                pass
+            elif user == '3':
+                pass
+        elif user == '2':
+            f.clear()
+            f.derive()
+            input()
+            continue
+    elif user == '2':
+        f.clear()
+        f.menu_2()
+        input()
+        continue
+    elif user == '3':
+        f.clear()
+        f.manual()
+        input()
+        continue
+    elif user == '4':
+        f.clear()
+        f.credits()
+        input()
+        continue
+    elif user == '5':
+        f.exit()
